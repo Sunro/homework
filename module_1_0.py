f1482214189ed82eb6acc6d2fe47605d4742225a
Sunro <sunroweb@gmail.com>
2024-11-11 01:37:45 +0300

    Clean

--- module_1_0.py
+++ module_1_0.py
@@ -7,18 +7,3 @@ age='24'
 print('New age:', age)
 is_student=True
 print('Is Student:', is_student)
-#print(is_student, type(is_student))
-s = u'\u221a'
-print(s, end="")
-print(25)
-
-#Практическая работа по уроку динамическая типизация
-name='Vladimir'
-print('Name:',name)
-age=36
-print('Age:',age)
-age='24'
-print('New age:', age)
-is_student=True
-print('Is Student:', is_student)
-print(is_student, type(is_student))
